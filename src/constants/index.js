83f97c30b414ca4ad3f958f2ddd4d6f1ddf33330
AlexandraHockett <alexandramandio@gmail.com>
2024-07-22 08:50:56 +0100

    done some colour changes and updated some projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,222 +1,183 @@
 import { meta, shopify, sheCodes, freecodecamp } from "../assets/images";
 import {
-    sun,
-    rain,
-    dictionary,
-    pinguin,
-    contact,
-    css,
-    estate,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    linkedin,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    react,
-    redux,
-    sass,
-    summiz,
-    tailwindcss,
-    typescript
+  sun,
+  rain,
+  dictionary,
+  pinguin,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  summiz,
+  tailwindcss,
+  typescript,
 } from "../assets/icons";
 
 export const skills = [
-    {
-        imageUrl: css,
-        name: "CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: express,
-        name: "Express",
-        type: "Backend",
-    },
-    {
-        imageUrl: git,
-        name: "Git",
-        type: "Version Control",
-    },
-    {
-        imageUrl: github,
-        name: "GitHub",
-        type: "Version Control",
-    },
-    {
-        imageUrl: html,
-        name: "HTML",
-        type: "Frontend",
-    },
-    {
-        imageUrl: javascript,
-        name: "JavaScript",
-        type: "Frontend",
-    },
-    {
-        imageUrl: mongodb,
-        name: "MongoDB",
-        type: "Database",
-    },
-    {
-        imageUrl: motion,
-        name: "Motion",
-        type: "Animation",
-    },
-    {
-        imageUrl: mui,
-        name: "Material-UI",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nextjs,
-        name: "Next.js",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nodejs,
-        name: "Node.js",
-        type: "Backend",
-    },
-    {
-        imageUrl: react,
-        name: "React",
-        type: "Frontend",
-    },
-    {
-        imageUrl: redux,
-        name: "Redux",
-        type: "State Management",
-    },
-    {
-        imageUrl: sass,
-        name: "Sass",
-        type: "Frontend",
-    },
-    {
-        imageUrl: tailwindcss,
-        name: "Tailwind CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: typescript,
-        name: "TypeScript",
-        type: "Frontend",
-    }
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
 ];
 
 export const certificates = [
-    {
-        name: "Responsive Web Design Certification",
-        organization: "FreeCodeCamp",
-        icon: freecodecamp,
-        iconBg: "#0A0A23",
-        imageUrl: "https://i.ibb.co/ckFSJ2N/Captura-de-ecr-2024-05-16-100856.png",
-        date: "March 26, 2024",
-        link: "https://www.freecodecamp.org/certification/AlexandraHockett/responsive-web-design",
-    },
-    {
-        name: "Advanced React Development",
-        organization: "SheCodes",
-        icon: sheCodes,
-        iconBg: "#F5F2FE",
-        imageUrl: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/049/007/original/49007.png?1713538731",
-        date: "September 2, 2022",
-        link:"https://www.shecodes.io/certificates/44fc432a69159fa02bae45245ef7af0d"
-    },
-    {
-        name: "Responsive Web Development",
-        organization: "SheCodes",
-        icon: sheCodes,
-        iconBg: "#F5F2FE",
-        imageUrl: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/042/017/original/42017.png?1713537710",
-        date: "October 24, 2021",
-        link:"https://www.shecodes.io/certificates/bcfcc457518b7725d6541e00a418d712"
-    
-    },
-    {
-        name: "Web Development",
-        organization: "SheCodes",
-        icon: sheCodes,
-        iconBg: "#F5F2FE",
-        imageUrl: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/035/608/original/35608.png?1713554952",
-        date: "August 17, 2021",
-        link:"https://www.shecodes.io/certificates/f92923ede3cb550b423dbe1cac2f4f6d"
-    },
-    {
-        name: "Introduction to Coding",
-        organization: "SheCodes",
-        icon: sheCodes,
-        iconBg: "#F5F2FE",
-        imageUrl: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/034/246/original/34246.png?1713536544",
-        date: "June 23, 2021",
-        link:"https://www.shecodes.io/certificates/67bf8e29a72f0ed3fbbc8182892c91ba"
-    },
+  {
+    name: "Responsive Web Design Certification",
+    organization: "FreeCodeCamp",
+    icon: freecodecamp,
+    iconBg: "#0A0A23",
+    imageUrl: "https://i.ibb.co/ckFSJ2N/Captura-de-ecr-2024-05-16-100856.png",
+    date: "March 26, 2024",
+    link: "https://www.freecodecamp.org/certification/AlexandraHockett/responsive-web-design",
+  },
+  {
+    name: "Advanced React Development",
+    organization: "SheCodes",
+    icon: sheCodes,
+    iconBg: "#F5F2FE",
+    imageUrl:
+      "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/049/007/original/49007.png?1713538731",
+    date: "September 2, 2022",
+    link: "https://www.shecodes.io/certificates/44fc432a69159fa02bae45245ef7af0d",
+  },
+  {
+    name: "Responsive Web Development",
+    organization: "SheCodes",
+    icon: sheCodes,
+    iconBg: "#F5F2FE",
+    imageUrl:
+      "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/042/017/original/42017.png?1713537710",
+    date: "October 24, 2021",
+    link: "https://www.shecodes.io/certificates/bcfcc457518b7725d6541e00a418d712",
+  },
+  {
+    name: "Web Development",
+    organization: "SheCodes",
+    icon: sheCodes,
+    iconBg: "#F5F2FE",
+    imageUrl:
+      "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/035/608/original/35608.png?1713554952",
+    date: "August 17, 2021",
+    link: "https://www.shecodes.io/certificates/f92923ede3cb550b423dbe1cac2f4f6d",
+  },
+  {
+    name: "Introduction to Coding",
+    organization: "SheCodes",
+    icon: sheCodes,
+    iconBg: "#F5F2FE",
+    imageUrl:
+      "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/034/246/original/34246.png?1713536544",
+    date: "June 23, 2021",
+    link: "https://www.shecodes.io/certificates/67bf8e29a72f0ed3fbbc8182892c91ba",
+  },
 ];
 
 export const socialLinks = [
-    {
-        name: 'Contact',
-        iconUrl: contact,
-        link: '/contact',
-    },
-    {
-        name: 'GitHub',
-        iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
-    },
-    {
-        name: 'LinkedIn',
-        iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
-    }
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
 ];
 
 export const projects = [
-    {
-        iconUrl: pinguin,
-        theme: 'btn-back-red',
-        name: 'My cute penguin',
-        description: 'Created a responsive webpage, complete with an intricately designed penguin image entirely crafted using HTML and CSS.',
-        link: 'https://mycutepenguin.netlify.app',
-    },
-    {
-        iconUrl: dictionary,
-        theme: 'btn-back-green',
-        name: 'Dictionary React APP',
-        description: 'Developed a Dictionary App using React.',
-        link: 'https://trusting-colden-46af2a.netlify.app/',
-    },
-    {
-        iconUrl: sun,
-        theme: 'btn-back-blue',
-        name: 'Weather App',
-        description: 'Developed a weather application using HTML, CSS, and JavaScript.',
-        link: 'https://theweatherhockettapp.netlify.app/',
-    },
-    {
-        iconUrl: rain,
-        theme: 'btn-back-pink',
-        name: 'Weather App (React)',
-        description: 'Built a weather application utilizing React.',
-        link: 'https://eloquent-payne-ad85ea.netlify.app/',
-    },
-    {
-        iconUrl: estate,
-        theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
-    },
-    {
-        iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
-        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
-    }
+  {
+    iconUrl: pinguin,
+    theme: "btn-back-red",
+    name: "My cute penguin",
+    description:
+      "Created a responsive webpage, complete with an intricately designed penguin image entirely crafted using HTML and CSS.",
+    link: "https://mycutepenguin.netlify.app/",
+  },
+  {
+    iconUrl: dictionary,
+    theme: "btn-back-green",
+    name: "Dictionary React APP",
+    description: "Developed a Dictionary App using React.",
+    link: "https://ahockettdictionary.netlify.app/",
+  },
+  {
+    iconUrl: sun,
+    theme: "btn-back-blue",
+    name: "Weather App",
+    description:
+      "Developed a weather application using HTML, CSS, and JavaScript.",
+    link: "https://theweatherhockettapp.netlify.app/",
+  },
+  {
+    iconUrl: rain,
+    theme: "btn-back-pink",
+    name: "Weather App (React)",
+    description: "Built a weather application utilizing React.",
+    link: "https://eloquent-payne-ad85ea.netlify.app/",
+  },
 ];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,40 +1,60 @@
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
-import { skills, certificates } from '../constants';
-import CTA from '../components/CTA';
+import { skills, certificates } from "../constants";
+import CTA from "../components/CTA";
 
 const About = () => {
   return (
-    <section className='max-container'>
-      <h1 className='head-text'>
-        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Alexandra</span>
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Alexandra
+        </span>
       </h1>
 
-      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p>
-          A Software Developer based in Portugal. In my journey of self-improvement, I've engaged in various courses, including Introduction to Coding, Web Development, Responsive Web Development, and Advanced React Development, all through SheCodes.
+          A Software Developer based in Portugal. In my journey of
+          self-improvement, I've engaged in various courses, including
+          Introduction to Coding, Web Development, Responsive Web Development,
+          and Advanced React Development, all through SheCodes.
         </p>
         <p>
-          Transitioning from customer-facing roles in hospitality and restaurants, I discovered a deep passion and aptitude for programming and web development. Driven by my desire for a career shift, I've dedicated myself to practical learning that mirrors real-world scenarios. My approach emphasizes pragmatism, with a keen awareness of creating tangible value in collaboration with like-minded professionals.
+          Transitioning from customer-facing roles in hospitality and
+          restaurants, I discovered a deep passion and aptitude for programming
+          and web development. Driven by my desire for a career shift, I've
+          dedicated myself to practical learning that mirrors real-world
+          scenarios. My approach emphasizes pragmatism, with a keen awareness of
+          creating tangible value in collaboration with like-minded
+          professionals.
         </p>
         <p>
-          My goal is to build a portfolio showcasing how my skills can effectively address your needs. My ethos revolves around a growth mindset, unwavering dedication, and a relentless pursuit of improvement. As evidence of my commitment, I've achieved several certificates, which I'm eager to leverage in contributing positively to your team.
+          My goal is to build a portfolio showcasing how my skills can
+          effectively address your needs. My ethos revolves around a growth
+          mindset, unwavering dedication, and a relentless pursuit of
+          improvement. As evidence of my commitment, I've achieved several
+          certificates, which I'm eager to leverage in contributing positively
+          to your team.
         </p>
       </div>
 
-      <div className='py-10 flex flex-col'>
-        <h3 className='subhead-text'>My Skills</h3>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
 
-        <div className='mt-16 flex flex-wrap gap-12'>
+        <div className="mt-16 flex flex-wrap gap-12">
           {skills.map((skill) => (
-            <div key={skill.name} className='block-container w-20 h-20'>
-              <div className='btn-back rounded-xl' />
-              <div className='btn-front rounded-xl flex justify-center items-center'>
+            <div key={skill.name} className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
                 <img
                   src={skill.imageUrl}
                   alt={skill.name}
-                  className='w-1/2 h-1/2 object-contain'
+                  className="w-1/2 h-1/2 object-contain"
                 />
               </div>
             </div>
@@ -42,52 +62,60 @@ const About = () => {
         </div>
       </div>
 
-      <div className='py-16'>
-        <h3 className='subhead-text'>Certificates</h3>
-        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
-          <p>Here are some certificates I've achieved, showcasing my commitment to continuous learning and proficiency in specific areas. They demonstrate my dedication and the skills I've acquired through various courses.</p>
+      <div className="py-16">
+        <h3 className="subhead-text">Certificates</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            Here are some certificates I've achieved, showcasing my commitment
+            to continuous learning and proficiency in specific areas. They
+            demonstrate my dedication and the skills I've acquired through
+            various courses.
+          </p>
         </div>
-        <div className='mt-12 flex'>
+        <div className="mt-12 flex">
           <VerticalTimeline>
             {certificates.map((certificate) => (
               <VerticalTimelineElement
                 key={certificate.name}
                 date={certificate.date}
                 icon={
-                  <div className='flex justify-center items-center w-full h-full'>
+                  <div className="flex justify-center items-center w-full h-full">
                     <img
                       src={certificate.icon}
                       alt={certificate.organization}
-                      className='w-[60%] h-[60%] object-contain'
+                      className="w-[60%] h-[60%] object-contain"
                     />
                   </div>
                 }
                 iconStyle={{ background: certificate.iconBg }}
                 contentStyle={{
-                  borderBottom: '8px',
-                  borderStyle: 'solid',
+                  borderBottom: "8px",
+                  borderStyle: "solid",
                   borderBottomColor: certificate.iconBg,
-                  boxShadow: 'none',
+                  boxShadow: "none",
                 }}
               >
                 <div>
-                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                  <h3 className="text-black text-xl font-poppins font-semibold">
                     {certificate.name}
                   </h3>
-                  <p className='text-black-500 font-medium font-base' style={{ margin: 0 }}>
+                  <p
+                    className="text-black-500 font-medium font-base"
+                    style={{ margin: 0 }}
+                  >
                     {certificate.organization}
                   </p>
                 </div>
                 <img
                   src={certificate.imageUrl}
                   alt={certificate.name}
-                  className='w-1/2 h-1/2 object-contain mx-auto mt-5'
+                  className="w-1/2 h-1/2 object-contain mx-auto mt-5"
                 />
                 <a
                   href={certificate.link}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='text-blue-600 hover:underline mt-2 block text-center'
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-purple-500 hover:underline mt-2 block text-center"
                 >
                   View Certificate
                 </a>
@@ -96,7 +124,7 @@ const About = () => {
           </VerticalTimeline>
         </div>
       </div>
-      <hr className='border-slate-200' />
+      <hr className="border-slate-200" />
       <CTA />
     </section>
   );
